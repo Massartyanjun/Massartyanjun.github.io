e1049accb8d94a0e5f095d3eac54003c1eb3ed3d
Yanjun <lyuyanjun@Yanjuns-MacBook-Pro.local>
2016-07-17 23:27:05 -0400

    hello

--- untitled folder/Yanjun.js
+++ untitled folder/Yanjun.js
@@ -15,7 +15,16 @@
 				$("#img5").click(function(){$("#audioplayer1").get(0).play()});
 			
 
-				
+				var safeColors = ['00','33','66','99','cc','ff'];
+				var rand = function() {
+    			return Math.floor(Math.random()*6);
+				};
+				var randomColor = function() {
+   			 	var r = safeColors[rand()];
+    			var g = safeColors[rand()];
+    			var b = safeColors[rand()];
+    			return "#"+r+g+b;
+				};
 
 
 				$(document).ready(function() {
@@ -26,6 +35,9 @@
     			});
 				});
 
+
+
+    		
 				
 			});
 			$(document).mousedown(function () {
